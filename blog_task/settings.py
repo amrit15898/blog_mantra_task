be0032e8b0fdd5ae06a103185bad4e352425b6bb
amrit15898 <apalsingh1581998@gmail.com>
2023-08-19 13:14:09 +0530

    allow all hosts

--- blog_task/settings.py
+++ blog_task/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j3w#ecq_g03gir9t69u!q6fnt@ovhv!^)0+l937*5r0nk0%3ol
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.9", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

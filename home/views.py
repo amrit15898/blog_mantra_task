33cedd61f03f267da46b0e563adea496fdec2e36
amrit15898 <apalsingh1581998@gmail.com>
2023-08-21 15:34:42 +0530

    ready to send

--- home/views.py
+++ home/views.py
@@ -28,6 +28,10 @@ def sign_up(requset):
             if user_obj:
                 messages.error(requset, "Try with different username this username alread exists")
                 redirect("sign-up")
+            
+            if len(password)<6:
+                messages.error(requset, "Password length must be 6")
+
 
             # Create the user object
             obj = User(first_name=first_name, last_name=last_name, username=username, email=email)
@@ -48,28 +52,29 @@ def login_page(request):
         password = request.POST.get("password")
         check_user = User.objects.filter(username = username).first()
         if not check_user:
-            messages.warning(request, "User does not exit")
-            return redirect("login/")
+            messages.error(request, "User does not exit")
+            return redirect("login")
         user = authenticate(request, username = username, password = password)
         if user is not None:
             login(request, user)
             return redirect("/")
             
         else:
-            messages.error("Invalid login credentails")
-            return redirect("login/")
+            messages.error(request, "Invalid login credentails")
+            return redirect("login")
 
 
     return render(request, "login.html")
 
 def home_page(request):
+    context = {}
     try:
         blogs = BlogPost.objects.all()
-        itmes_per_page = 3
+        itmes_per_page = 5
         paginator  = Paginator(blogs, itmes_per_page)  
         page_no = request.GET.get("page")
         page_obj = paginator.get_page(page_no)
-        context = {"blogs": page_obj} 
+        context["blogs"] =  page_obj
     except Exception as e:
         messages.error(f"An error occured {e}")
     return render(request, "home.html", context)
@@ -87,11 +92,23 @@ def detail_blog(request, id):
     if request.method == "POST":
         if "send_email" in request.POST:
             recipient_email = request.POST.get("recipient-email")
-            send_mail_to_user("Desi Coders", blog_detail_url, "none76845@gmail.com", recipient_email)
+            send_mail_to_user("Codigo Mantra", f"Read the best blgo of the universe {blog_detail_url}", "none76845@gmail.com", recipient_email)
+        elif "delete_comment" in request.POST:
+            comment_id = request.POST.get("comment_id")
+            try:
+                comment = Comment.objects.get(id=comment_id)
+                if comment.author == request.user:
+                    comment.delete()
+                    messages.success(request, "Comment deleted successfully.")
+                else:
+                    messages.error(request, "You can only delete your own comments.")
+            except Comment.DoesNotExist:
+                messages.error(request, "Comment does not exist.")
         elif request.user.is_authenticated:
             comment = request.POST.get("cmnt")
             obj = Comment(Blog=blog, author=request.user, content=comment)
             obj.save()
+        
         else:
             messages.error(request, "Please log in to post a comment.")
     
@@ -113,10 +130,11 @@ def like_comment(request, id):
 
         if already_liked:
             comment.like.remove(user)
-            messages.success(request, "Comment unliked successfully.")
+            
         else:
             comment.like.add(user)
-            messages.success(request, "Comment liked successfully.")
+            
+    
     else:
         messages.error(request, "Please log in before liking a comment.")
     return redirect(request.META.get('HTTP_REFERER', '/'))

ad984186bbbdc39f43a0cbdc91fe2788d2085719
amrit15898 <apalsingh1581998@gmail.com>
2024-09-06 19:11:06 +0530

    fix issue again

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/blog_mantra_task/blog/settings.py
+sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/blog_mantra_task/blog_task/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
